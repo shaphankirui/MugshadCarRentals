f3096aea2d5ba059703298008bd8ef8041ea3cd2
shaphankirui <79963010+shaphankirui@users.noreply.github.com>
2023-12-16 11:40:00 +0300

    push

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from '../service/auth.service';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent {
   registerData = {
